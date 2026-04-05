f2b2fec049e95ed6069ef6bea648f30c887701d4
alvaro22c <condorialvaro2020@gmail.com>
2026-04-05 19:20:06 -0300

    Delete src directory

--- src/cifrado_8.py
+++
@@ -1,8 +0,0 @@
-def CifradoCesar(mensaje,desplazamiento):
-    for i in mensaje:
-        if i.isalpha():
-            letra = ord("A")if i.isupper() else ord("a")
-            desplazar = chr((ord(i)-letra+desplazamiento)%26 + letra)
-            print(desplazar,end="")
-        else:
-            print(i,end="")
